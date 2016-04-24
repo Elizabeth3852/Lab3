637611b6fcb0b7b6c8be6a7aa7d9f512be678ee8
Elizabeth3852 <omelnitskaya.liza@mail.ru>
2016-04-24 20:22:18 +0300

    abstract class

---
+++ л.р.3/л.р.3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace л.р._3
+{
+    // Класс транспортное средство
+    abstract class Vehicle
+    {
+        private int price;      // стоимость тр. средства
+        private int year;       // год выпуска
+        private int maxspeed;   // максимальная скорость
+        private int curspeed;   // текущая скорость
+
+        public int Price // свойство Стоимости
+        {
+            get { return price; }
+            set { if (value > 0) price = value; }
+        }
+        public int YearOfCreation // свойство Года выпуска
+        {
+            get { return year; }
+            set { if (value > 1900 && value <= DateTime.Today.Year) year = value; }
+        }
+        public int MaxSpeed // свойство Максимальной скорости
+        {
+            get { return maxspeed; }
+            set { if (value > 0) maxspeed = value; }
+        }
+        public int CurrentSpeed
+        {
+            get { return curspeed; }
+            set { if (value > maxspeed) throw OutOfMaxBorder; curspeed = value; }
+        }
+        public virtual void SpeedUp()   // метод увеличивает текущую скорость, может переопределятся
+        {
+            CurrentSpeed++;
+        }
+        public virtual void SpeedDown() // метод уменьшает текущую скорость, может переопределятся
+        {
+            CurrentSpeed--;
+        }
+        // исключения для классов наследников:
+        protected Exception NonBellowZero = new Exception("Исключение, введенное значение не может быть отрицательным!");
+        protected Exception OutOfMaxBorder = new Exception("Исключение, превышена максимальна граница!");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
